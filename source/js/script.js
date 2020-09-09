ebee747578d947e37a0f6de8d8712ee087bc8f3b
panica913@gmail.com <panica913@gmail.com>
2020-09-09 22:08:31 +0300

    блоки табов и цен в 3 версиях + модалка

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,15 @@
 
 var pageHeader = document.querySelector('.page-header');
 var headerToggle = document.querySelector('.page-header__toggle');
+var tabs = document.querySelector('.countrys');
+var buttons = tabs.querySelectorAll('.countrys__tab-item');
+var items = tabs.querySelectorAll('.countrys__item');
+var modalButton = document.querySelectorAll('.modal-button');
+var modal = document.querySelector('.modal');
+var modalCloseButton = document.querySelector('.modal__close');
+var overlay = document.createElement('div');
+
+overlay.className = 'overlay';
 
 pageHeader.classList.remove('page-header--nojs');
 
@@ -12,5 +21,56 @@ headerToggle.addEventListener('click', function () {
   } else {
     pageHeader.classList.add('page-header--closed');
     pageHeader.classList.remove('page-header--opened');
+  }
+});
+
+function change(arr, i) {
+  arr.forEach(function (item) {
+    item.forEach(function (k) {
+      k.classList.remove('is-active');
+    });
+    item[i].classList.add('is-active');
+  });
+}
+
+var onTabClick = function onTabClick(i) {
+  buttons[i].addEventListener('click', function () {
+    change([buttons, items], i);
+  });
+};
+
+for (var i = 0; i < buttons.length; i++) {
+  onTabClick(i);
+}
+
+var onModalButtonClick = function () {
+  if (modal.classList.contains('modal-show') === true) {
+    modal.classList.remove('modal-show');
+    document.body.removeChild(overlay);
+  }
+
+  document.body.appendChild(overlay);
+  parent.className = 'overlay';
+  modal.classList.add('modal-show');
+};
+
+for (var j = 0; j < modalButton.length; j++) {
+  modalButton[j].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    onModalButtonClick();
+  });
+}
+
+overlay.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  document.body.removeChild(overlay);
+  modal.classList.remove('modal-show');
+});
+
+modalCloseButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  if (modal.classList.contains('modal-show')) {
+    modal.classList.remove('modal-show');
   }
 });
